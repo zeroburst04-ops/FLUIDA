3f2aa5a584bd1c69d5bda87146978083290710cb
Ferrusia <ziminior18@gmail.com>
2026-05-17 01:13:08 +0700

    MADE OPTIONS WORK

--- option.gd
+++ option.gd
@@ -1,5 +1,10 @@
 extends Control
 
-
+func _process(delta: float) -> void:
+	%Music_Player.volume_db =  (($BackGround_Music_SO.value * 24) - 22)
+	
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
+
+func _on_music_player_finished() -> void:
+	%Music_Player.play()
