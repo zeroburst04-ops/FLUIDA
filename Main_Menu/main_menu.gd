e92a2717829c5293bb128ce8ca82a0eaba3da71a
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-18 23:08:17 +0700

    fck music

--- Main_Menu/main_menu.gd
+++ Main_Menu/main_menu.gd
@@ -1,8 +1,7 @@
 extends Control
 
-
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://levels_menu.tscn")
 	
 func _on_tutorial_button_pressed() -> void:
 	pass # Replace with function body.
---
+++ levels_menu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
