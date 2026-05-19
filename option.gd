683123ce9809c57c3c6e76fb8bd050cb24d1d876
Ferrusia <ziminior18@gmail.com>
2026-05-20 01:44:05 +0700

    Brightness overlay, and Saved settigns

--- option.gd
+++ option.gd
@@ -1,9 +1,18 @@
 extends Control
 
-func _process(delta: float) -> void:
-	Settings.BackGround_Music = (($BackGround_Music_SO.value * 24) - 15)
-	%Music_Player.volume_db = Settings.BackGround_Music
+func _ready() -> void:
+	%Brightness_SO.value = Settings.Brightness_Slider
+	%BackGround_Music_SO.value = Settings.BackGround_Music_Slider
 	
+func _process(delta: float) -> void:
+	Settings.BackGround_Music_Slider = %BackGround_Music_SO.value
+	Settings.Brightness_Slider = %Brightness_SO.value
+	
+	Settings.BackGround_Music = ((%BackGround_Music_SO.value * 24) - 15)
+	Settings.Brightness = 1 - %Brightness_SO.value 
+	
+	%Music_Player.volume_db = Settings.BackGround_Music
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
 
--- settings.gd
+++ settings.gd
@@ -1,3 +1,7 @@
 extends Node
 
+var BackGround_Music_Slider : float = 1
+var Brightness_Slider : float = 1
+
 var BackGround_Music : float = 18.
+var Brightness : float = 0
