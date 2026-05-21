9362e36359f4340b84e6d58db1eca3ec2abc970f
Ferrusia <ziminior18@gmail.com>
2026-05-22 00:04:39 +0700

    FINAL PUSH

--- Pipes/TS_Kyogre/kyogre_cup.gd
+++ Pipes/TS_Kyogre/kyogre_cup.gd
@@ -3,7 +3,8 @@ extends Node2D
 @onready var Kyoger : = %Kyogre
 @onready var Start : float = Kyoger.global_position.y
 @onready var Threshold : float = Start - 30
-
+@onready var Pipe_Area : = %Pipe_Area
+var Is_Overlapping : bool = true
 var timer : float = 6
 
 func _ready() -> void:
@@ -21,7 +22,26 @@ func _process(delta: float) -> void:
 		timer = 6
 		%Win_Label.text = "3"
 		%Win_Label.visible = false
+	
+# Only call Overlap if Pipe_Area was successfully found
+	if Pipe_Area: 
+		Overlap()
 
+func Overlap() -> void:
+	if Pipe_Area == null:
+		return
+		
+	# 2. Check if the array contains any overlapping areas
+	if Pipe_Area.get_overlapping_areas().size() > 1:
+		# Areas are overlapping: Remove green and blue (turns the shape Red)
+		%CupShape.modulate.g = 0.0
+		%CupShape.modulate.b = 0.0
+		Is_Overlapping = true
+	else:
+		# No overlaps: Reset green and blue to normal (turns the shape back to White/Normal)
+		%CupShape.modulate.g = 255
+		%CupShape.modulate.b = 255
+		Is_Overlapping = false
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("OUT_OF_BOUNDS"):
--- Pipes/WaterCup/water_cup.gd
+++ Pipes/WaterCup/water_cup.gd
@@ -1,6 +1,7 @@
-
 extends Node2D
 
+@onready var Pipe_Area : = %Pipe_Area
+var Is_Overlapping : bool = false
 enum Plugs {
 	Bottom,
 	Bottom_Left,
@@ -29,3 +30,22 @@ func _ready() -> void:
 			%"Upper-Left_Plug".modulate.a = 0
 			%UpperLeftPlug_Collision.disabled = true
 			
+func _process(delta: float) -> void:
+	if Pipe_Area: 
+		Overlap()
+
+func Overlap() -> void:
+	if Pipe_Area == null:
+		return
+		
+	# 2. Check if the array contains any overlapping areas
+	if Pipe_Area.get_overlapping_areas().size() > 0:
+		# Areas are overlapping: Remove green and blue (turns the shape Red)
+		%CupShape.modulate.g = 0.0
+		%CupShape.modulate.b = 0.0
+		Is_Overlapping = true
+	else:
+		# No overlaps: Reset green and blue to normal (turns the shape back to White/Normal)
+		%CupShape.modulate.g = 255
+		%CupShape.modulate.b = 255
+		Is_Overlapping = false
