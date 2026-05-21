5afea317b26845f2f7a68b4a118110db82490fe9
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-21 13:01:47 +0700

    tf is ts bro

--- option.gd
+++ option.gd
@@ -1,9 +1,9 @@
 extends Control
-
+"""
 func _ready() -> void:
 	%Brightness_SO.value = Settings.Brightness_Slider
 	%BackGround_Music_SO.value = Settings.BackGround_Music_Slider
-	
+
 func _process(delta: float) -> void:
 	Settings.BackGround_Music_Slider = %BackGround_Music_SO.value
 	Settings.Brightness_Slider = %Brightness_SO.value
@@ -12,7 +12,8 @@ func _process(delta: float) -> void:
 	Settings.Brightness = 1 - %Brightness_SO.value 
 	
 	%Music_Player.volume_db = Settings.BackGround_Music
-
+	"""
+	
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
 
