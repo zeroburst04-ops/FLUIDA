55c6a7075a95cf509147a2ec59596ef6bdaf61d6
Ferrusia <ziminior18@gmail.com>
2026-05-21 23:41:20 +0700

    Kyogre cup gimmick and refixed everything lmao

--- Pipes/TS_Kyogre/kyogre_cup.gd
+++ Pipes/TS_Kyogre/kyogre_cup.gd
@@ -2,12 +2,28 @@ extends Node2D
 
 @onready var Kyoger : = %Kyogre
 @onready var Start : float = Kyoger.global_position.y
-@onready var Threshold : float = Start - 70
+@onready var Threshold : float = Start - 30
+
+var timer : float = 6
 
 func _ready() -> void:
 	%Win_Label.visible = false
 	
 func _process(delta: float) -> void:
 	if Kyoger.global_position.y < Threshold:
-		LevelData.I_Win = true
+		timer -= delta
 		%Win_Label.visible = true
+		%Win_Label.text = str(int(timer))
+		if timer < 1:
+			%Win_Label.text = "YOU WIN !!"
+			LevelData.I_Win = true
+	else:
+		timer = 6
+		%Win_Label.text = "3"
+		%Win_Label.visible = false
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("OUT_OF_BOUNDS"):
+		LevelData.I_Lose = false
+		
--- level_data.gd
+++ level_data.gd
@@ -2,3 +2,4 @@ extends Node
 
 var I_Running : bool = false
 var I_Win : bool = false
+var I_Lose : bool = false
