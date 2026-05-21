bca1eb8e576b0d9a50ed4c707ce4e077921dc874
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-21 18:24:00 +0700

    .

--- loading__screen.gd
+++ loading__screen.gd
@@ -10,4 +10,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	%Music.volume_db = Settings.BackGround_Music
+	pass
