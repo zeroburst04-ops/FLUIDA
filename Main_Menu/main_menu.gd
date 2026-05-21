00fc282e345ea78393ba73d6d4e4770a4c55276d
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-21 14:29:18 +0700

    adfb

--- Main_Menu/main_menu.gd
+++ Main_Menu/main_menu.gd
@@ -8,7 +8,7 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels_menu.tscn")
 	
 func _on_tutorial_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://loading _screen.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
