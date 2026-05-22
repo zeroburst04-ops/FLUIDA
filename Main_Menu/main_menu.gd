14b5848ada4b14fdfd036b3f749f0215e9fbbb1f
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-22 21:38:03 +0700

    0

--- Main_Menu/main_menu.gd
+++ Main_Menu/main_menu.gd
@@ -1,10 +1,8 @@
 extends Control
 
-@onready var W = %kyogre_cry
 @onready var BGM = %BackGround_Music
 func _process(delta: float) -> void:
 	BGM.volume_db = Settings.BackGround_Music
-	W.volume_db = Settings.BackGround_Music
 	
 @onready var Brightness = $Brightness_Overlay
 func _physics_process(delta:float) -> void:
@@ -25,11 +23,3 @@ func _on_settings_button_pressed() -> void:
 func _on_test_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://TestScene/test_scene.tscn")
 	
-func _ready():
-	$BackGround_Music.volume_db = 5.5
-	$kyogre_cry.volume_db = -10.0
-	await get_tree().create_timer(5.0).timeout
-	$kyogre_cry.play()
-	while true:
-		$kyogre_cry.play()
-		await get_tree().create_timer(18.0).timeout 
