4f839bf6c129d269d2b4b389d1098de49872071e
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-23 07:23:28 +0700

    update

--- win_scene.gd
+++ win_scene.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$BackButton.hide() 
+	await get_tree().create_timer(4.0).timeout # Wait for 2 seconds
+	$BackButton.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,3 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels_menu.tscn")
+	
