2bc46c34964ec172832054e445c6c47f10ab28b2
Ferrusia <ziminior18@gmail.com>
2026-05-24 18:11:46 +0700

    DONE

--- LEVELS/camera_2d.gd
+++ LEVELS/camera_2d.gd
@@ -1,27 +1,38 @@
 extends Camera2D
 
-@export var scroll_speed: float = 400.0
+@export var scroll_speed: float = 200
 @export var smooth_speed: float = 10.0
 
 var target_y: float
-var Pipe_panel 
+var pipe_panel_start_y: float
+var Camera_start_y : float
+var Camera_end_y : float
+
+var Cam_mentok_atas : bool = false
+var Cam_mentok_bawah : bool = false
 
 func _ready() -> void:
-	Pipe_panel = %Pipe_panel.global_position.y
-	target_y = global_position.y
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			target_y += scroll_speed
-		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			target_y -= scroll_speed
-		
-		# Automatically clamp using your Inspector Limit properties
-		target_y = clamp(target_y, limit_top, limit_bottom)
-
-func _process(delta: float) -> void:
+	pipe_panel_start_y = %Pipe_panel.global_position.y
+	Camera_start_y = %Camera2D.global_position.y
+	Camera_end_y = %Camera2D.global_position.y + 7500
 	
-	global_position.y = lerp(global_position.y, target_y, smooth_speed * delta)
-	if %Pipe_panel.global_position.y < Pipe_panel:
-		%Pipe_panel.global_position.y = Pipe_panel
+func _process(delta: float) -> void:
+	if %Camera2D.global_position.y < Camera_start_y:
+		%Camera2D.global_position.y = Camera_start_y
+	if %Pipe_panel.global_position.y < pipe_panel_start_y:
+		%Pipe_panel.global_position.y = pipe_panel_start_y
+	
+	if %Camera2D.global_position.y <= Camera_start_y:
+		Cam_mentok_atas = true
+	else:
+		Cam_mentok_atas = false
+	
+	if %Camera2D.global_position.y >= Camera_end_y:
+		Cam_mentok_bawah = true
+	else:
+		Cam_mentok_bawah = false
+		
+	if Input.is_action_just_pressed("Go_up") and not Cam_mentok_atas:
+		%Camera2D.position.y -= scroll_speed
+	if Input.is_action_just_pressed("Go_Down") and not Cam_mentok_bawah:
+		%Camera2D.position.y += scroll_speed
