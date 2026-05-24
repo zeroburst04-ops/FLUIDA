2ea1cf0bd4da9c2de359ad388e1a918e50ef4ef8
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-24 10:21:03 +0700

    sd

--- LEVELS/level_1.gd
+++ LEVELS/level_1.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
-@onready var Wintext := %Win_state
-@onready var Overlap := %Overlap_text
-@onready var Pipe_parent := %Pipes
+@onready var Wintext : = %Win_state
+@onready var Overlap : = %Overlap_text
 
-var overlap_text_timer : float = 0.0
+@onready var Pipe_parent : = %Pipes
+
+var overlap_text_timer : float = 0.000000000000
+
+
 var last_ticks: float = 0.0
 
 func _ready() -> void:
-	# Allows this script to run while Engine.time_scale is 0
-	process_mode = Node.PROCESS_MODE_ALWAYS 
-	
+	process_mode = Node.PROCESS_MODE_ALWAYS # <-- ADD THIS LINE
 	LevelData.I_Running = false
 	%Overlap_text.visible = false
-	Engine.time_scale = 0.0
+	Engine.time_scale = 0.
 	Snapfunc()
 	last_ticks = Time.get_ticks_msec() 
 	
@@ -28,30 +29,27 @@ func _process(delta: float) -> void:
 		return
 	if LevelData.I_Lose:
 		Wintext.text = "ぉせ"
-		return
+		pass
 
-	if has_node("%FPS"):
-		%FPS.text = str(Engine.get_frames_per_second()) + "FPS"
+	%FPS.text = str(Engine.get_frames_per_second()) + "FPS"
 	
 	if %Overlap_text.visible:
-		overlap_text_timer += unscaled_delta
-		if overlap_text_timer >= 1.0:
-			overlap_text_timer = 0.0
+		overlap_text_timer += unscaled_delta # Use our unscaled_delta here instead!
+		print(overlap_text_timer)
+		if overlap_text_timer >= 1:
+			overlap_text_timer = 0
 			%Overlap_text.visible = false
 			
 func _on_start_pressed() -> void:
-	# Reset timer whenever start is pressed to prevent state carryover
-	overlap_text_timer = 0.0 
-	
 	for i in range(%Pipes.get_child_count()):
 		var Pipe = %Pipes.get_child(i)
-		# Note: Ensure 'Is_Overlapping' is spelled exactly like this in your Pipe script
 		if Pipe.Is_Overlapping:
 			%Overlap_text.visible = true
+			
 			return
 			
 	LevelData.I_Running = true
-	Engine.time_scale = 1.0
+	Engine.time_scale = 1
 
 func Snapfunc() -> void:
 	global_position.x -= int(global_position.x) % 50
