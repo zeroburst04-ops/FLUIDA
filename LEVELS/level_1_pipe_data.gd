d6cf882cc002c28d12e95a1d829050a6dcda5d8f
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-25 17:03:57 +0700

    level 1 done

--- LEVELS/level_1_pipe_data.gd
+++ LEVELS/level_1_pipe_data.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 var pipe_inventory: Dictionary = {
-	"TS_Knee": 3,
-	"TS_Socket": 1,
-	"TS_Socket_Reducer": 2,
-	"TS_Tee": 67
+	"TS_Knee": 2,
+	"TS_Socket": 2,
+	"TS_Socket_Reducer": 1,
+	"TS_Tee": 1
 }
