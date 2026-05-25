1ceee48df60c18bc4136443b68e9213f1b7a6e11
Ferrusia <ziminior18@gmail.com>
2026-05-25 15:41:53 +0700

    fixed kyogre

--- levels_menu.gd
+++ levels_menu.gd
@@ -20,3 +20,7 @@ func _on_level_1_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://TestScene/test_scene.tscn")
+
+
+func _on_level_2_button_down() -> void:
+	get_tree().change_scene_to_file("res://LEVELS/level_2.tscn")
