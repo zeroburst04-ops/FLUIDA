76607407f32739ed34002fa10edc711281cfd5bf
Ferrusia <ziminior18@gmail.com>
2026-05-26 13:52:28 +0700

    LEVEL 1 AND LEVEL 2 AHAHAHAHAHAHAHAH

--- levels_menu.gd
+++ levels_menu.gd
@@ -6,7 +6,20 @@ func _physics_process(delta:float) -> void:
 
 func _process(delta: float) -> void:
 	%Music.volume_db = Settings.BackGround_Music
-	
+	if Input.is_action_just_pressed("1"):
+		get_tree().change_scene_to_file("res://SANE_LEVELS/SANE_LEVEL_1/level_1_SL.tscn")
+	if Input.is_action_just_pressed("2"):
+		get_tree().change_scene_to_file("res://SANE_LEVELS/SANE_LEVEL_2/level_2_SL.tscn")
+	if Input.is_action_just_pressed("3"):
+		pass
+	if Input.is_action_just_pressed("4"):
+		pass
+	if Input.is_action_just_pressed("5"):
+		pass
+
+
+
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
 
