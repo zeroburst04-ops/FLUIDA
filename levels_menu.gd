caf7ae9ee9b92d211350a77dddd4abf501430f2e
zeroburst04-ops <zeroburst04@gmail.com>
2026-05-27 17:22:27 +0700

    Update levels_menu.gd

--- levels_menu.gd
+++ levels_menu.gd
@@ -18,7 +18,7 @@ func _on_level_1_pressed() -> void:
 	scenemanager.load_scene("res://LEVELS/level_1.tscn")
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://TestScene/test_scene.tscn")
+	scenemanager.load_scene("res://TestScene/test_scene.tscn")
 
 func _on_level_2_pressed() -> void:
 	scenemanager.load_scene("res://LEVELS/level_2.tscn")
